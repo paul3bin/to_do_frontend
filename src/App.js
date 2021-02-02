f06bb22620865266d81c9639b97258ab45641d34
Ebin Paul <ebinpaul@outlook.com>
2021-02-02 22:50:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, {useState, useEffect} from 'react';
 import { useCookies } from 'react-cookie';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import {Redirect} from 'react-router-dom'
 
 import { TasksList } from './components/list-tasks';
 import {API} from './api-service'
@@ -18,11 +19,9 @@ function App() {
   const [tasks, setTasks] = useState([]);
 
   const [newTask, setNewTask] = useState('');
-
-  const user = tasks[0]
   
   useEffect(() => {
-    if(!token['token']) window.location.href = '/'
+    if(!token['token']) window.location.href = '/' 
   }, [token])
 
   const logoutUser = () => {
@@ -40,17 +39,16 @@ function App() {
   }
 
   const addNewTask = () => {
-    if (newTask.length >200){
-      alert('Task length cannot be greater than 200.')
+    if (newTask.length >50){
+      alert('Task length cannot be greater than 50.')
     }
     else if (newTask.length === 0){
       alert('Empty task cannot be added!')
     }
     else{
-      const newTaskAdded = [...tasks, newTask]
-      setTasks(newTaskAdded)
-      API.addTask({task: newTask, user: userID['id']},token['token']).catch(error => console.log(error))
-      // API.getTasks(token['token']).then(resp => setTasks(resp))
+      API.addTask({task: newTask, user: userID['id']},token['token'])
+      .then(resp => setTasks([...tasks, resp]))
+      .catch(error => console.log(error))
       setNewTask('')
     }
   }
@@ -63,14 +61,14 @@ function App() {
           <FontAwesomeIcon icon={faSignOutAlt} className="Icons" onClick={logoutUser}/>
         </div>
       </nav>
+      <div className='addTaskComponent'>
+        <input type="text" placeholder='New Task' value={newTask} onChange={evnt => setNewTask(evnt.target.value)}/>
+        <button className='btn btn-success' onClick={addNewTask}>Add New Task</button>
+      </div>
       <div>
         <TasksList 
           tasks={tasks}
           updateTaskList={updateTaskListAction}/>
-      </div>
-      <div className='addTaskComponent'>
-        <input type="text" placeholder='New Task' value={newTask} onChange={evnt => setNewTask(evnt.target.value)}/>
-        <button className='btn btn-success' onClick={addNewTask}>Add New Task</button>
       </div>
     </div>
   );
