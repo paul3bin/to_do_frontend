08d096243bd10c3d5b009f670eb5bc0c115bee01
Ebin Paul <ebinpaul@outlook.com>
2021-02-06 12:54:59 +0530

    Update user_register.js

--- src/components/user_register.js
+++ src/components/user_register.js
@@ -1,74 +1,101 @@
-import React, {useState} from 'react';
-import {API} from '../api-service';
-import {Link} from 'react-router-dom';
-import {auth} from '../components/auth';
+import React, { useState } from "react";
+import { API } from "../api-service";
+import { Link } from "react-router-dom";
+import { auth } from "../components/auth";
 
-function RegisterUser(){
+function RegisterUser() {
+  document.title = "Register User";
 
-    document.title = 'Register User';
+  const [username, setUsername] = useState("");
 
-    const [username, setUsername] = useState('');
-    
-    const [password, setPassword] = useState('');
+  const [password, setPassword] = useState("");
 
-    const [passwordReEnter, setPasswordReEnter] = useState('');
+  const [passwordReEnter, setPasswordReEnter] = useState("");
 
-    const isDisabled = username.length===0 || password.length===0 || passwordReEnter.length===0;
+  const isDisabled =
+    username.length === 0 ||
+    password.length === 0 ||
+    passwordReEnter.length === 0;
 
-    const passwordCheck = () =>{
-        return (/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/.test(password))
+  const passwordCheck = () => {
+    return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/.test(password);
+  };
+
+  const passwordReEnterCheck = () => {
+    return password.length === passwordReEnter.length;
+  };
+
+  const registerClicked = () => {
+    if (passwordCheck && passwordReEnterCheck) {
+      API.registerUser({ username, password }).catch((error) =>
+        console.log(error)
+      );
+      alert("New user registered. Now, login with the same credentials.");
+      window.location.href = "/";
+    } else {
+      alert("Check your password and try again.");
     }
+  };
 
-    const passwordReEnterCheck = () =>{
-        return (password.length===passwordReEnter.length)
-    }
+  return (
+    <div className="login-body">
+      <div className="container-fluid">
+        <div className="row justify-content-center">
+          <div className="col-12 col-sm-6 col-md-3">
+            <form className="form-container">
+              <h4 className="text-center fw-bold fst-italic">Register</h4>
 
-    const registerClicked = () => {
-        if (passwordCheck && passwordReEnterCheck){
-            API.registerUser({username, password})
-            .catch(error => console.log(error))
-            alert('New user registered. Now, login with the same credentials.')
-            window.location.href = '/'
-        }
+              <div class="form-group">
+                <input
+                  id="UserName"
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter Username"
+                  value={username}
+                  onChange={(evnt) => setUsername(evnt.target.value)}
+                />
+              </div>
 
-        else{
-            alert('Check your password and try again.')
-        }
-    }
+              <div class="form-group">
+                <input
+                  type="password"
+                  class="form-control"
+                  id="password"
+                  placeholder="Enter password"
+                  value={password}
+                  onChange={(evnt) => setPassword(evnt.target.value)}
+                />
+              </div>
 
-    return (
-        <div className='login-body'>
-            <div className='container-fluid'>
-                <div className='row justify-content-center'>
-                    <div className='col-12 col-sm-6 col-md-3'>
-                    <form className='form-container'>
-                        
-                        <h4 className='text-center fw-bold fst-italic'>Register</h4>
-                        
-                        <div class="form-group">
-                            <input id='UserName' type='text' className="form-control" placeholder='Enter Username' value={username} 
-                            onChange={evnt => setUsername(evnt.target.value)}/>
-                        </div>
-                        
-                        <div class="form-group">
-                            <input type="password" class="form-control" id="password" placeholder='Enter password' 
-                            value={password} onChange={evnt => setPassword(evnt.target.value)}/>
-                        </div>
+              <div class="form-group">
+                <input
+                  type="password"
+                  class="form-control"
+                  id="password_reEnter"
+                  placeholder="Re-enter Password"
+                  value={passwordReEnter}
+                  onChange={(evnt) => setPasswordReEnter(evnt.target.value)}
+                />
+              </div>
 
-                        <div class="form-group">
-                            <input type="password" class="form-control" id="password_reEnter" placeholder='Re-enter Password' 
-                            value={passwordReEnter} onChange={evnt => setPasswordReEnter(evnt.target.value)}/>
-                        </div>
-  
-                        <button disabled={isDisabled} type="submit" class="btn btn-primary btn-block" onClick={registerClicked}>Register</button>
+              <button
+                disabled={isDisabled}
+                type="submit"
+                class="btn btn-primary btn-block"
+                onClick={registerClicked}
+              >
+                Register
+              </button>
 
-                        <p className='p-login-register'>Already have an account? Login <Link to="/">here</Link>.</p> 
-                    </form>
-                    </div>
-                </div>
-            </div>
+              <p className="p-login-register">
+                Already have an account? Login <Link to="/">here</Link>.
+              </p>
+            </form>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export {RegisterUser};
+export { RegisterUser };
