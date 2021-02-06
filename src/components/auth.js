d8253b1dd8300d3a8fc1678e511f15ae74a119a5
Ebin Paul <ebinpaul@outlook.com>
2021-02-06 12:54:53 +0530

    Update auth.js

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,68 +1,87 @@
-import React, { useState, useEffect } from 'react';
-import { useCookies } from 'react-cookie';
-import {Link} from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useCookies } from "react-cookie";
+import { Link } from "react-router-dom";
 
-import { API } from '../api-service';
+import { API } from "../api-service";
 
-function Auth(){
+function Auth() {
+  document.title = "Login";
 
-    document.title = 'Login';
+  const [username, setUsername] = useState("");
 
-    const [username, setUsername] = useState('');
-    
-    const [password, setPassword] = useState('');
+  const [password, setPassword] = useState("");
 
-    const isDisabled = username.length===0||password.length===0;
+  const isDisabled = username.length === 0 || password.length === 0;
 
-    const [token, setToken] = useCookies(['token']);
+  const [token, setToken] = useCookies(["token"]);
 
-    const [userID, setUserID] = useCookies(['id']);
+  const [userID, setUserID] = useCookies(["id"]);
 
-    const loginClicked = () => {
+  const loginClicked = () => {
+    API.loginUser({ username, password })
+      .then((resp) => [setToken("token", resp.token), setUserID("id", resp.id)])
+      .catch((error) => console.log(error));
+  };
 
-        API.loginUser({username, password})
-        .then(resp => [setToken('token', resp.token), setUserID('id', resp.id)])
-        .catch(error => console.log(error))
+  useEffect(() => {
+    if (token["token"] === "undefined") {
+      alert("Wrong username or password.");
+      setToken("token", "");
+      setUsername("");
+      setPassword("");
+    } else {
+      if (token["token"]) window.location.replace("/tasks");
     }
+  }, [token]);
 
-    useEffect( () => {
-        if (token['token']==='undefined'){
-            alert('Wrong username or password.')
-            setToken('token', '')
-            setUsername('')
-            setPassword('')
-        }
-        else{
-            if(token['token']) window.location.replace('/tasks');
-        }
-    }, [token])
+  return (
+    <div className="login-body">
+      <div className="container-fluid">
+        <div className="row justify-content-center">
+          <div className="col-12 col-sm-6 col-md-3">
+            <form className="form-container">
+              <h4 className="text-center fw-bold">Login</h4>
+              <div class="form-group">
+                <input
+                  type="email"
+                  class="form-control"
+                  id="username"
+                  placeholder="Enter username"
+                  value={username}
+                  onChange={(evnt) => setUsername(evnt.target.value)}
+                />
+              </div>
 
-    return (
-        <div className='login-body'>
-            <div className='container-fluid'>
-                <div className='row justify-content-center'>
-                    <div className='col-12 col-sm-6 col-md-3'>
-                    <form className='form-container'>
-                        <h4 className='text-center fw-bold'>Login</h4>
-                        <div class="form-group">
-                            <input type="email" class="form-control" id="username" placeholder='Enter username'
-                            value={username} onChange={evnt => setUsername(evnt.target.value)}/>
-                        </div>
-                        
-                        <div class="form-group">
-                            <input type="password" class="form-control" id="password" placeholder='Enter password' 
-                            value={password} onChange={evnt => setPassword(evnt.target.value)}/>
-                        </div>
-  
-                        <button disabled={isDisabled} type="submit" class="btn btn-primary btn-block" onClick={loginClicked}>Submit</button>
+              <div class="form-group">
+                <input
+                  type="password"
+                  class="form-control"
+                  id="password"
+                  placeholder="Enter password"
+                  value={password}
+                  onChange={(evnt) => setPassword(evnt.target.value)}
+                />
+              </div>
 
-                        <p className='p-login-register'>Don't have an account? Register <Link to="/register-user">here</Link>.</p>
-                    </form>
-                    </div>
-                </div>
-            </div>
+              <button
+                disabled={isDisabled}
+                type="submit"
+                class="btn btn-primary btn-block"
+                onClick={loginClicked}
+              >
+                Submit
+              </button>
+
+              <p className="p-login-register">
+                Don't have an account? Register{" "}
+                <Link to="/register-user">here</Link>.
+              </p>
+            </form>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export {Auth};
+export { Auth };
