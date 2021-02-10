ae037951d5733351bf13c70d8f6ea199f6ae4084
Ebin Paul <ebinpaul@outlook.com>
2021-02-10 11:43:24 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {Route, BrowserRouter} from 'react-router-dom';
-import {CookiesProvider} from 'react-cookie';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Route, BrowserRouter } from "react-router-dom";
+import { CookiesProvider } from "react-cookie";
 
-import {Auth} from './components/auth';
-import {RegisterUser} from './components/user_register';
-import {ProtectedRoute} from './ProtectedRoute';
+import { Auth } from "./components/auth";
+import { RegisterUser } from "./components/user_register";
+import { ProtectedRoute } from "./ProtectedRoute";
 
-
-function Router(){
-  return(
+function Router() {
+  return (
     <React.StrictMode>
       <CookiesProvider>
         <BrowserRouter>
-          <Route exact path="/" component={Auth}/>
-          <ProtectedRoute exact path="/tasks" component={App}/>
-          <Route exact path="/register-user" component={RegisterUser}/>
+          <Route exact path="/" component={Auth} />
+          <ProtectedRoute exact path="/tasks" component={App} />
+          <Route exact path="/register-user" component={RegisterUser} />
         </BrowserRouter>
       </CookiesProvider>
     </React.StrictMode>
-  )
+  );
 }
 
-ReactDOM.render(<Router />,document.getElementById('root'));
+ReactDOM.render(<Router />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
